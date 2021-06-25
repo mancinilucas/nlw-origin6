809153c361abb7e0d139e3521e5897012492ba7a
mancinilucas <mancini.lucasg@gmail.com>
2021-06-25 13:24:19 -0300

    NLW Day 4

--- main.js
+++ main.js
@@ -18,16 +18,17 @@ for (const link of links) {
 }
 
 // mudar o header da página quando der scroll //
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
 // testimonials carousel slider swiper //
 const swiper = new Swiper('.swiper-container', {
@@ -53,6 +54,24 @@ scrollReveal.reveal(
   #services .image, #services .card,
   #testimonials header, #testimonials .testimonials
   #contact .text, #contact .links
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
+
+// Botão 'voltar para o top' //
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+// When Scroll //
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
